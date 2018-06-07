e70138a881a0032a153301ec51e3c91b55b9ee63
Chris Dunn <chris@cidean.com>
2018-06-07 18:03:51 -0400

    Add Core Data  Models

---
+++ src/Cidean.GatherHub/Core/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cidean.GatherHub.Core.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string MeetingTimes { get; set; }
+        public CourseCategory Category { get; set; }
+
+    }
+}

3556a3948c5494d067ca69002850f9c50200cb86
Chris Dunn <chris@cidean.com>
2018-06-21 15:40:38 -0400

    Add repository pattern to data layer

---
+++ src/Cidean.GatherHub/Core/Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cidean.GatherHub.Core.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task<T> GetById(int id);
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void Delete(T entity);
+
+    }
+}
---
+++ src/Cidean.GatherHub/Core/Data/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cidean.GatherHub.Core.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly DbContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+            this._dbSet = dbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            return _dbSet.FindAsync(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry<T>(entity).State = EntityState.Modified;
+        }
+        public void Delete(int id)
+        {
+            T entity = _dbSet.FindAsync(id).Result;
+            Delete(entity);
+        }
+        public virtual void Delete(T entity)
+        {
+            if (_dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+            _dbSet.Remove(entity);
+        }
+
+    }
+}

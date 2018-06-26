a5f501d0ea2e96fdbc8154d648e301ad9db44e69
Chris Dunn <chris@cidean.com>
2018-06-26 15:50:40 -0400

    Add UnitofWork design pattern to data layer

---
+++ src/Cidean.GatherHub/Core/Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Cidean.GatherHub.Core.Models;
+using System.Threading.Tasks;
+
+namespace Cidean.GatherHub.Core.Data
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Course> Courses { get; }
+        IRepository<CourseCategory> CourseCategories { get; }
+
+        Task Save();
+        void Dispose();
+    }
+}
---
+++ src/Cidean.GatherHub/Core/Data/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cidean.GatherHub.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cidean.GatherHub.Core.Data
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly HubContext _hubContext;
+        public IRepository<Course> Courses { get; private set; }
+        public IRepository<CourseCategory> CourseCategories { get; private set; }
+
+        public UnitOfWork(HubContext hubContext)
+        {
+            //build repositories
+            this._hubContext = hubContext;
+            Courses = new Repository<Course>(_hubContext);
+            CourseCategories = new Repository<CourseCategory>(_hubContext);
+        }
+
+        public async Task Save()
+        {
+            await _hubContext.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!this.disposed)
+            {
+                if(disposing)
+                {
+                    _hubContext.Dispose();
+                }
+            }
+
+            this.disposed = true;
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
